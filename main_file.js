45368411d92f8ed9d28337ff2deb618380e9d420
ReginaRichie <regina20006@mail.ru>
2022-04-03 10:44:55 +0300

    some changes for Evgen

--- main_file.js
+++ main_file.js
@@ -1,6 +1,16 @@
-
 document.getElementById('button_log_in').onclick = () => {
-    document.getElementById('modalka').style.display = 'none'
+    document.getElementById('modal').style.display = 'flex'
 }
 
+document.getElementById('close_app').onclick = () => {
+    document.getElementById("modal").style.display = 'none'
+}
+
+document.getElementById('button_registration').onclick = () => {
+    document.getElementById("modal_registration").style.display = 'flex'
+}
+
+document.getElementById('close_app_2').onclick = () => {
+    document.getElementById("modal_registration").style.display = 'none'
+}
 
