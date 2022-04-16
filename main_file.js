080851e9414e2d52e63ce7f93db85610adc14359
ReginaRichie <regina20006@mail.ru>
2022-04-16 20:20:42 +0300

    some changes for Evgen

--- main_file.js
+++ main_file.js
@@ -17,3 +17,23 @@ document.getElementById('close_app_2').onclick = () => {
 document.getElementById('login_button').onclick = () => {
     window.location.replace("chat_page/chat_page.html")
 }
+
+
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function (){
+        if (this.readyState === 4 && this.status === 200){
+            document.getElementById("login_button").innerHTML = this.responseText
+        }
+    }
+    xmlhttp.open("GET", "http://ev-gen.ru:10000", true);
+    xmlhttp.send();
+
+    xmlhttp.onload = function(){
+        if (xmlhttp.status !== 200){
+            alert( 'Ошибка: ' + xmlhttp.status);
+        }else{
+            alert('Все ок' + xmlhttp.response)
+        }
+    }
+}
